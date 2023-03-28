c31abd8acbbbf3783f44078ab20526f025ec7dd2
E Luckie <eluckie.d@gmail.com>
2023-03-27 20:30:12 -0600

    add base properties to Place

--- TravelApi/Models/Place.cs
+++ TravelApi/Models/Place.cs
@@ -3,5 +3,12 @@ namespace TravelApi.Models
   public class Place
   {
     public int PlaceId { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string SeasonVisited { get; set; }
+    public int Rating { get; set; }
+    public string Review { get; set; }
+    public string User_Name { get; set; }
   }
 }
